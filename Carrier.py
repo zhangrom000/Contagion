6bf41d24cd4be51044ec493105982368c9fd14ac
Zhathos <austenh22@hotmail.com>
2017-05-18 15:00:35 -0700

    Added init, update and infect methods. Updated variables

--- Carrier.py
+++ Carrier.py
@@ -1,9 +1,28 @@
 
 class Carrier(object):
     #### Variables ####
-    NUM_OF_RATS #Number of rats in this carrier
-    INFECTED #Bool
+    NUM_IN_SWARM #Number of carriers in this carrier
     MOBILITY #Ability to travel
+    INFECTED #Bool
+    INFECTION_RATE #Infectious rate, higher rate means more likely to infect.
     LIFESPAN_COUNTER #Counts the lifespan of the rats.  Becoming infected will cut the lifespan.
-    POP_GROWTH_RATE #Growth rate of this agent. The agent will eventually split.
+    MAX_SWARM_SIZE #Max number in swarm before the agent splits.
+    POP_GROWTH_RATE #Percentage growth rate of this agent. The agent will eventually split.
+    x # x location in grid
+    y # y location in grid
     
+    def __init__(self, x_init=0, y_init=0, swarm_init = 1, infected="False"):
+        self.x = x_init
+        self.y = y_init
+        self.NUM_IN_SWAM = swarm_init
+        self.INFECTED = infected
+    
+    def update(self):
+        self.NUM_IN_SWARM = self.NUM_IN_SWARM * (1 + POP_GROWTH_RATE)
+        pass
+    
+    def Infect(self):
+        INFECTED = True
+        pass
+        
+       
