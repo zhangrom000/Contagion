fe55790dd8bf6ccedce0164aed348ff59dfcb5e9
Zhathos <austenh22@hotmail.com>
2017-05-18 18:19:26 -0700

    Update Carrier.py

--- Carrier.py
+++ Carrier.py
@@ -1,3 +1,11 @@
+""""
+Carrier contains a Swarm population that will be used in calculating the infection potential.
+Swarms will grow exponentially until they reach the max swarm. Once this occurs then the swarm will split off
+
+@author Austen Harp
+@version 5/18/2017
+Made for use in the Contagion 
+""""
 
 class Carrier(object):
     #### Variables ####
@@ -11,12 +19,14 @@ class Carrier(object):
     x # x location in grid
     y # y location in grid
     
-    def __init__(self, x_init=0, y_init=0, swarm_init = 1, infected="False"):
+    def __init__(self, x_init=0, y_init=0, swarm_init = 1, infected="False", infect_rate=0.2):
         self.x = x_init
         self.y = y_init
         self.NUM_IN_SWAM = swarm_init
         self.INFECTED = infected
+        self.INFECTION_RATE = infect_rate
     
+    # Update population of swarm
     def update(self):
         self.NUM_IN_SWARM = self.NUM_IN_SWARM * (1 + POP_GROWTH_RATE)
         pass
