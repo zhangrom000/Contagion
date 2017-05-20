e1134b29672780b229e5b0c48f6377cfe4b54236
Zhathos <austenh22@hotmail.com>
2017-05-20 15:42:56 -0700

    Basic grid created

--- Visualize.py
+++ Visualize.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy as N
+import matplotlib
 
 class Visualize(object):
     
-    def plot_env_grid(env_grid):
+    def plot_env_grid(): #env_grid, list_of_carriers, show = False):
+        
         """
         Plots a given grid, which is a 2D array of cells.
         
@@ -29,6 +32,49 @@ class Visualize(object):
         #loop through each cell in grid and give color based on cell data
         #plot with matplotlib
         #update color hues/shading based on cell data change
+    
+        width = 15 #env_grid.width
+        length = 20 #env_grid.width
+        env_grid = np.zeros(shape = (length, width))
+        print(env_grid)
+        env_grid.fill(2)
+        env_grid.astype(int)
+        print(env_grid)
+    
+        data = N.zeros((width, length, 3), dtype='f')
+        cmap = ['k', 'b', 'g', '0.5', '0.1']
+        convert = matplotlib.colors.ColorConverter()
+        
+        
+        if len(env_grid) != 0:       #- data for a non-empty farm
+            #for i in env_grid:
+            for i in range(0, width):
+                for j in range(0, length):
+                    cellVal = int(env_grid[j, i])
+                    
+                    temp = N.array( convert.to_rgb( cmap[cellVal] ) )
+                    data[i, j, :] = temp[:]
+                
+        else:                              #- data for empty farm
+            pass
+        
+        
+        #- Create figure and axes objects:
+    
+        fig, ax = plt.subplots(1, 1)
+    
+    
+        #- Plot the image, turn off the axes labels, and (maybe) show the plot:    
+        ax.imshow(data, interpolation='none',
+                extent=[0, width, 0, length],
+                zorder=0)
+        ax.axis('off')    
+        plt.show()
+    
+    
+        #- Return values:    
+        return fig, ax
+    
         
     def plot_top_grid(top_grid, list_of_carriers=[]):
         """
@@ -48,3 +94,5 @@ class Visualize(object):
         #update dot movement/merging as they travel through env_grids
         #update dot color fade as carrier data changes
         
+    
+    plot_env_grid()
