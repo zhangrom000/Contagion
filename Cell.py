9c734f3fe44f2781175c244a0f7cfa788a3ac4b6
Roman <zroman@uw.edu>
2017-05-20 16:20:07 -0700

    Cell Update
    update_population implemented
    TODO: Anything having to do with lifespan,quarantine, or recovery
    
    infect_probability method: basic formula for how likely the infection
    will spread

--- Cell.py
+++ Cell.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.random as rand
 """
 Class Cell
 
@@ -11,7 +12,7 @@ the simulation.
 """
 class Cell(object):
     
-    def __init__(self,xLoc, yLoc, density):
+    def __init__(self,xLoc = 0, yLoc = 0, density = 'Land', lifespan = 14, quarantined = 'False'):
         #### Variables ####
         if (density == 'Urban'):
             self.TOTAL_POP = 1000
@@ -36,9 +37,10 @@ class Cell(object):
         self.RECOVER_PERCENTAGE = 0.05 #Percentage of population recovered after end of lifespan
         self.TOTAL_DEAD = 0#Total Dead Population of cell
         self.TOTAL_INFECTED = 0 #Total Infected Population of cell
-        self.LIFESPAN = 14 #Lifespan (in days) an infected human. 
-        self.QUARANTINED = false #Whether or not this cell is blocked off from contagion
+        self.LIFESPAN = lifespan #Lifespan (in days) an infected human. 
+        self.QUARANTINED = quarantined #Whether or not this cell is blocked off from contagion
         self.INFECTED_ARR = [] #Keeps track of the number of infected per time step
+        self.arrayIterator = 0
 
     """
     update_population:
@@ -50,13 +52,51 @@ class Cell(object):
         This cell's pollution value 
     
     """
-    def update_population(self):
+    def update_population(self, carriersInGrid):
         """
         PSEUDOCODE: 
         Check environment value, agent state at the coordinate of this Cell 
         in order to determine incrementation of infected population within this
         time step. Increment number of dead based on infected lifespan. 
         """
-    
-    
         
+        if self.TOTAL_INFECTED == 0:
+            for carrier in carriersInGrid:
+                if (carrier.x == self.xPos and carrier.y == self.ypos):
+                    #compare random value between 1 and 0 to infect_probability
+                    #if rand val is less than probability, 
+                    # TOTAL_INFECTED ++
+                    randVal = rand.random()
+                    if (randVal < self.infect_probability):
+                        self.TOTAL_INFECTED += 1
+                    
+        elif (self.TOTAL_INFECTED > 0 and self.TOTAL_INFECTED < self.TOTAL_POPULATION):
+            randVal = rand.random()
+            #generate random value 1-0, 
+            #if value < infect_probability, increment numInfected by Arbitrary amt
+            if (randVal < self.infect_probability):
+                self.TOTAL_INFECTED += 2
+        else:
+            self.INFECTED_ARR[self.arrayIterator] = self.TOTAL_INFECTED
+            self.arrayIterator += 1
+    
+    def infect_probability(self):
+        probability = 0.0
+        if  self.TOTAL_POPULATION > 1000:
+            probability += 0.5
+        elif self.TOTAL_POPULATION < 1000 and self.TOTAL_POPULATION > 100:
+            probability += 0.35
+        elif self.TOTAL_POPULATION < 100 and self.TOTAL_POPULATION > 10:
+            probability += 0.2
+        else:
+            probability += 0.15
+            
+        if self.POLLUTION > 0.8:
+            probability += 0.2
+            
+        elif self.POLLUTION > 0.5:
+            probability += 0.1
+        else: 
+            probability += 0.05
+            
+        return probability
