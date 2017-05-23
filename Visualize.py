f9e9e0b6336b8b436db1d6d82fbec3502fdee27d
Zhathos <austenh22@hotmail.com>
2017-05-23 16:20:50 -0700

    Fixed Carrier. The dot is slightly offcenter now

--- Visualize.py
+++ Visualize.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as N
 import matplotlib
+import time as time
 
 
 """
@@ -25,8 +26,7 @@ Environment Outlines:
         
 class Visualize(object):
         
-    def plot_all(self, gridObj, show = True):
-        
+    def init_plot(self, gridObj):
         height = gridObj.GRID_HEIGHT
         width = gridObj.GRID_WIDTH
         Grid = gridObj.GRID        
@@ -97,7 +97,80 @@ class Visualize(object):
                 zorder=0)
         axarr[2].axis('off')    
         axarr[2].set_title("Infected Grid") 
+        
+        plt.ion()
+        plt.show()
+        
+        return f, axarr
+        
+    
+    
+    def update_plot(self, gridObj, f, axarr):
+        
+        height = gridObj.GRID_HEIGHT
+        width = gridObj.GRID_WIDTH
+        Grid = gridObj.GRID        
+        dataEnv = N.zeros((width, height, 3), dtype='f')
+        dataPop = N.zeros((width, height, 3), dtype='f')
+        dataInf = N.zeros((width, height, 3), dtype='f')
+        # 0 = Land, 1 = City, 2 = Suburban, 3 = Rural, 4 = Barrier
+        cEnvMap = ['#8B4513', '#808080', 'g', '#FFA500', '#800080']
+        # Hex Gradient in 10 steps from green to red
+        cInfMap = ['#4BF740', '#5DE13D', '#6FCB3B', '#81B638', '#93A036', '#A58B33', '#B77531', '#C95F2E', '#DB4A2C', '#FF1F27']
+        # Hex Gradient in 5 steps from white to black
+        cPopMap = ['#FFFFFF', '#BFBFBF', '#7F7F7F', '#3F3F3F', '#000000']
+        convert = matplotlib.colors.ColorConverter()
+        
+        # Setup the Environment color grid
+        if len(Grid) != 0:
+            #for i in env_grid:
+            for i in range(0, width):
+                for j in range(0, height):
+                    # Grab the current cell
+                    cell = Grid[j, i]
+                           
+                    # Setup the Environment color grid
+                    temp = N.array( convert.to_rgb( cEnvMap[cell.ENV_TYPE] ) )
+                    dataEnv[i, j, :] = temp[:]
 
+                    # Setup the Population color grid
+                    if cell.INITIAL_POP == 0: popRatio = 0
+                    else: popRatio = cell.TOTAL_DEAD / cell.INITIAL_POP
+                    popColor = int(popRatio * 4) # convert pop ratio to integer
+                    if popColor > 4: popColor = 4
+                    temp = N.array( convert.to_rgb( cPopMap[popColor] ) ) # grab the hex code belonging to that pop ratio
+                    dataPop[i, j, :] = temp[:] 
+
+
+                    # Setup the Infected color grid
+                    if cell.TOTAL_POP == 0: infRatio = 0
+                    else: infRatio = cell.TOTAL_INFECTED / cell.TOTAL_POP
+                    infColor = int(infRatio * 9) # convert inf ratio to integer
+                    if infColor > 9: infColor = 9
+                    temp = N.array( convert.to_rgb( cInfMap[infColor] ) ) # grab the hex code belonging to that inf ratio
+                    dataInf[i, j, :] = temp[:]  
+                    
+        else: # error catch for empty grid
+            pass
+        
+        
+        #- Plot each grid    
+        axarr[0].imshow(dataEnv, interpolation='none',
+                extent=[0, width, 0, height],
+                zorder=0) 
+
+           
+        axarr[1].imshow(dataPop, interpolation='none',
+                extent=[0, width, 0, height],
+                zorder=0)
+
+
+        axarr[2].imshow(dataInf, interpolation='none',
+                extent=[0, width, 0, height],
+                zorder=0)
+        
+        plt.pause(0.4)
+        #f.canvas.draw()
         plt.show()
         
     def plot_carriers(self, gridObj, list_of_carriers=[], show=True):
@@ -116,6 +189,7 @@ class Visualize(object):
         #add color to dots
         #update dot movement/merging as they travel through env_grids
         #update dot color fade as carrier data changes
+        
         height = gridObj.GRID_HEIGHT
         width = gridObj.GRID_WIDTH
         
@@ -123,8 +197,9 @@ class Visualize(object):
         carriers_y = np.zeros(len(list_of_carriers))
         
         for i in range(len(list_of_carriers)):
-            carriers_x[i] = width - list_of_carriers[i].x
-            carriers_y[i] = height - list_of_carriers[i].y
+            carriers_x[i] = list_of_carriers[i].x
+            carriers_y[i] = width - list_of_carriers[i].y
         
-        plt.scatter(carriers_x, carriers_y, c='y')        
+        scat = plt.scatter(carriers_x, carriers_y, c='y')        
         plt.show()
+        return scat     
