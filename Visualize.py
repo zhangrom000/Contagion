52958848d539991d40fb7bebc8de6a93e90f7d15
Zhathos <austenh22@hotmail.com>
2017-05-22 18:51:54 -0700

    Now plots 2 grids, pop and env
    Will not work until Grid has been updated to use an array of cells. Once that has been changed, the env grid will need a way to see what envrionment the current cell belongs to.

--- Visualize.py
+++ Visualize.py
@@ -3,74 +3,96 @@ import matplotlib.pyplot as plt
 import numpy as N
 import matplotlib
 
+
+"""
+Plots a given grid, which is a 2D array of cells.
+
+Pop cell Filling:        
+    100% alive = solid green
+    100% infected = solid red
+    100% dead (0 population) = black
+    
+Cell fades from green to red as population gets infected.
+Cell fades to darker color as population dies.
+
+Environment Outlines:
+    Suburban: green
+    Urban: grey
+    Rural: orange
+    Land: brown
+    Barrier: purple
+"""
+        
 class Visualize(object):
-    
-    def plot_env_grid(self, env_grid, show = False): #env_grid, list_of_carriers, show = False):
         
-        """
-        Plots a given grid, which is a 2D array of cells.
+    def plot_all(self, gridObj, show = True):
         
-        Each cell has a color, each envGrid has an outline color.
+        # Below is me trying to figure out how to split this method into different functions
+        #     So far havent gotten them to work. 
+        #envFig, envAx = plt.subplots()  
+        #popFig, popAx = self.plot_pop_grid(gridObj, False)
+        #envFig, envAx = self.plot_env_grid(gridObj, False)
+        #f, axarr = plt.subplots(2)
+        #envFig, (envAx, popAx) = plt.subplots(2, sharex=True, sharey=True)        
+        #popFig, popAx = plt.subplots()
         
-        Cell Filling:        
-            100% alive = solid green
-            100% infected = solid red
-            100% dead (0 population) = black
-            
-        Cell fades from green to red as population gets infected.
-        Cell fades to darker color as population dies.
-        
-        Environment Outlines:
-            Suburban: green
-            Urban: grey
-            Rural: orange
-            Land: brown
-            Barrier: purple
-        """
-        
-        #take env_grid and give the border an outline
-        #loop through each cell in grid and give color based on cell data
-        #plot with matplotlib
-        #update color hues/shading based on cell data change
-    
-        height = env_grid.GRID_HEIGHT
-        width = env_grid.GRID_WIDTH
-        Grid = env_grid.GRID
-    
-        data = N.zeros((width, height, 3), dtype='f')
+        height = gridObj.GRID_HEIGHT
+        width = gridObj.GRID_WIDTH
+        Grid = gridObj.GRID        
+        dataEnv = N.zeros((width, height, 3), dtype='f')
+        dataPop = N.zeros((width, height, 3), dtype='f')
         # 0 = Land, 1 = City, 2 = Suburban, 3 = Rural, 4 = Barrier
         cmap = ['#8B4513', '#808080', 'g', '#FFA500', '#800080']
+        # Hex Gradient in 10 steps from green to red
+        cPopMap = ['#4BF740', '#5DE13D', '#6FCB3B', '#81B638', '#93A036', '#A58B33', '#B77531', '#C95F2E', '#DB4A2C', '#FF1F27']
         convert = matplotlib.colors.ColorConverter()
         
         
-        if len(Grid) != 0:       #- data for a non-empty farm
+        if len(Grid) != 0:
             #for i in env_grid:
             for i in range(0, width):
                 for j in range(0, height):
                     cellVal = int(Grid[j, i])                    
                     temp = N.array( convert.to_rgb( cmap[cellVal] ) )
-                    data[i, j, :] = temp[:]
+                    dataEnv[i, j, :] = temp[:]
                 
-        else:                              #- data for empty farm
+        else: # error catch for empty grid
+            pass
+
+        # Setup the Population color grid
+        if len(Grid) != 0:
+            #for i in env_grid:
+            for i in range(0, width):
+                for j in range(0, height):
+                    cell = Grid[j, i]
+                    popRatio = cell.TOTAL_INFECTED / cell.TOTAL_POP
+                    popColor = int(popRatio * 9) # convert pop ratio to integer
+                    temp = N.array( convert.to_rgb( cPopMap[popColor] ) ) # grab the hex code belonging to that pop ratio
+                    dataPop[i, j, :] = temp[:]
+                
+        else: # error catch for empty grid
             pass
         
         
-        #- Create figure and axes objects:
-    
-        fig, ax = plt.subplots(1, 1)
+        #- Create figure and axes objects:        
+        f, axarr = plt.subplots(1, 2) 
     
     
         #- Plot the image, turn off the axes labels, and (maybe) show the plot:    
-        ax.imshow(data, interpolation='none',
+        axarr[0].imshow(dataEnv, interpolation='none',
                 extent=[0, width, 0, height],
                 zorder=0)
-        ax.axis('off')    
-        if show:
-            plt.show()
-    
-        print("Reached!")   
-        return fig, ax
-    
+        axarr[0].axis('off')    
+        axarr[0].set_title("Environment Grid") 
+
+           
+        axarr[1].imshow(dataPop, interpolation='none',
+                extent=[0, width, 0, height],
+                zorder=0)
+        axarr[1].axis('off')    
+        axarr[1].set_title("Population Grid") 
+
+        plt.show()
         
     def plot_top_grid(top_grid, list_of_carriers=[]):
         """
@@ -90,5 +112,3 @@ class Visualize(object):
         #update dot movement/merging as they travel through env_grids
         #update dot color fade as carrier data changes
         
-    
-#    plot_env_grid(env_grid)
