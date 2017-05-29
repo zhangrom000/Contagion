76989097c06e682f250517470fb072fbb41576a1
Zhathos <austenh22@hotmail.com>
2017-05-29 12:40:19 -0700

    Fixed a few gradient bugs
    Added comments, removed redundant code, added error catches

--- Visualize.py
+++ Visualize.py
@@ -9,8 +9,9 @@ import time as time
 Plots a given grid, which is a 2D array of cells.
 
 Pop cell Filling:        
-    100% alive = solid green
-    100% infected = solid red
+    100% Healthy = solid green
+    100% Infected = solid red
+    100% alive = white
     100% dead (0 population) = black
     
 Cell fades from green to red as population gets infected.
@@ -21,7 +22,6 @@ Environment Outlines:
     Urban: grey
     Rural: orange
     Land: brown
-    Barrier: purple
 """
         
 class Visualize(object):
@@ -63,8 +63,8 @@ class Visualize(object):
 
 
                     # Setup the Infected color grid
-                    if cell.TOTAL_SUSCEPTIBLE == 0: infRatio = 0
-                    else: infRatio = cell.TOTAL_INFECTED / cell.TOTAL_SUSCEPTIBLE
+                    if cell.TOTAL_POP == 0: infRatio = 0
+                    else: infRatio = cell.TOTAL_INFECTED / cell.TOTAL_POP
                     infColor = int(infRatio * 9) # convert inf ratio to integer
                     temp = N.array( convert.to_rgb( cInfMap[infColor] ) ) # grab the hex code belonging to that inf ratio
                     dataInf[i, j, :] = temp[:]  
@@ -105,7 +105,7 @@ class Visualize(object):
         plt.plot([], 'ks', label="100% Dead")
         plt.plot([], 'gs', label="Healthy")
         plt.plot([], 'rs', label="Infected")
-        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0., fontsize = 8)
 
         f.canvas.set_window_title('Contagion')
         
@@ -124,13 +124,15 @@ class Visualize(object):
         dataEnv = N.zeros((width, height, 3), dtype='f')
         dataPop = N.zeros((width, height, 3), dtype='f')
         dataInf = N.zeros((width, height, 3), dtype='f')
+        
+        cEmpty = '#8B4513'
+        cDead = '#000000'
         # 0 = Land, 1 = City, 2 = Suburban, 3 = Rural, 4 = Barrier
-        cEnvMap = ['#8B4513', '#808080', 'g', '#FFA500', '#800080']
+        cEnvMap = [cEmpty, '#808080', 'g', '#FFA500', '#800080']
         # Hex Gradient in 10 steps from green to red
         cInfMap = ['#4BF740', '#5DE13D', '#6FCB3B', '#81B638', '#93A036', '#A58B33', '#B77531', '#C95F2E', '#DB4A2C', '#FF1F27']
         # Hex Gradient in 10 steps from white to black
-        cPopMap = ['#FFFFFF', '#E2E2E2', '#C6C6C6', '#AAAAAA', '#8D8D8D', '#717171', '#555555', '#383838', '#1C1C1C', '#000000']
-        cEmpty = '#40352C'
+        cPopMap = ['#FFFFFF', '#E2E2E2', '#C6C6C6', '#AAAAAA', '#8D8D8D', '#717171', '#555555', '#383838', '#1C1C1C', cDead]
         convert = matplotlib.colors.ColorConverter()
         
         # Setup the Environment color grid
@@ -151,22 +153,22 @@ class Visualize(object):
                     else: 
                         popRatio = float(cell.TOTAL_DEAD) / float(cell.INITIAL_POP)
                         popColor = int(popRatio * 9) # convert pop ratio to integer
-                        if popColor > 9 or popColor < 0: popColor = 9
+                        if popColor > 9 or popColor < 0: popColor = 9 # Error check for out of bounds
+                        if popColor == 9 and cell.INITIAL_POP != cell.TOTAL_DEAD: popColor = 8 # If theres humans left, dont be black
                         temp = N.array( convert.to_rgb( cPopMap[popColor] ) ) # grab the hex code belonging to that pop ratio
-
-#                    if cell.TOTAL_DEAD != 0: print(str(cell.TOTAL_DEAD) + " dead and initial: " + str(cell.INITIAL_POP) + " and ratio: " + str(popRatio))
+                        
                     dataPop[i, j, :] = temp[:] 
 
 
                     # Setup the Infected color grid
-                    if cell.INITIAL_POP == 0: 
-                        temp = N.array( convert.to_rgb( cEmpty ) ) # grab the hex code belonging to that pop ratio
-                    else: 
-                        if cell.TOTAL_SUSCEPTIBLE == 0: infRatio = 0
-                        else: infRatio = float(cell.TOTAL_INFECTED) / float(cell.TOTAL_SUSCEPTIBLE + cell.TOTAL_INFECTED + cell.TOTAL_RECOVERED)
-                        #if cell.TOTAL_INFECTED != 0 and cell.TOTAL_SUSCEPTIBLE != 0: print(str(cell.TOTAL_INFECTED) + " inf and TOTAL_SUSCEPTIBLE: " + str(cell.TOTAL_SUSCEPTIBLE) + " and ratio: " + str(infRatio))
+                    if cell.INITIAL_POP == 0:  # Check if the cell is land
+                        temp = N.array( convert.to_rgb( cEmpty ) )                      
+                    elif cell.INITIAL_POP == cell.TOTAL_DEAD: # Check if all dead
+                        temp = N.array( convert.to_rgb( cDead ) )                       
+                    else:
+                        infRatio = float(cell.TOTAL_INFECTED + cell.TOTAL_DEAD) / float(cell.TOTAL_POP + cell.TOTAL_RECOVERED + cell.TOTAL_INFECTED + cell.TOTAL_DEAD)
                         infColor = int(infRatio * 9) # convert inf ratio to integer
-                        if infColor > 9 or infColor < 0: infColor = 9
+                        if infColor > 9 or infColor < 0: infColor = 9 # Error check for out of bounds
                         temp = N.array( convert.to_rgb( cInfMap[infColor] ) ) # grab the hex code belonging to that inf ratio
 
                     dataInf[i, j, :] = temp[:]
@@ -190,7 +192,7 @@ class Visualize(object):
                 extent=[0, width, 0, height],
                 zorder=0)
         
-        #plt.pause(0.4)
+        plt.pause(0.4)
         plt.show()
         
     def plot_carriers(self, gridObj, list_of_carriers=[], show=True):
