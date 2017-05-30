f9ac86e950695f9af448992c308909ff731c2e3c
GBK4 <grat.king@gmail.com>
2017-05-29 18:26:06 -0700

    Recovered array implement

--- Cell.py
+++ Cell.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import numpy.random as rand
 from Carrier import Carrier
 """
@@ -28,15 +29,15 @@ class Cell(object):
         #### Variables ####
         if (density == 'City'):
             self.ENV_TYPE = 1
-            self.INITIAL_POP = 1000
+            self.INITIAL_POP = 10000 * random.uniform(0.75, 1.25)
             self.DENSITY = 1
         elif(density == 'Suburban'):
             self.ENV_TYPE = 2
-            self.INITIAL_POP = 100
+            self.INITIAL_POP = 1000 * random.uniform(0.75, 1.25)
             self.DENSITY = 0.8
         elif(density == 'Rural'):
             self.ENV_TYPE = 3
-            self.INITIAL_POP = 10
+            self.INITIAL_POP = 100 * random.uniform(0.75, 1.25)
             self.DENSITY = 0.4
         elif(density == 'Land'):
             self.ENV_TYPE = 0
@@ -60,6 +61,8 @@ class Cell(object):
         self.LIFESPAN = lifespan #Lifespan (in days) an infected human. 
         self.QUARANTINED = quarantined #Whether or not this cell is blocked off from contagion
         self.INFECTED_ARR = np.zeros(self.LIFESPAN) #Keeps track of the number of infected per time step
+        self.REINFECTION = 45
+        self.RECOVERED_ARR = np.zeros(self.REINFECTION)
         self.carrierList = carrierList #Array of Carriers at the same x-y coordinate as this Cell
         self.currentTimeStep = 0 #The current deltaX of the simulation
         
@@ -81,11 +84,20 @@ class Cell(object):
         self.carrierList = carriers #Update the list of carriers for those that might have moved to this Cell
         self.currentTimeStep += 1
         
+        
+        ## Reinfect ##
+        unrecovered = self.RECOVERED_ARR[self.currentTimeStep % self.REINFECTION]
+        self.RECOVERED_ARR[self.currentTimeStep % self.REINFECTION] = 0
+        self.TOTAL_RECOVERED -= unrecovered
+        self.TOTAL_SUSCEPTIBLE += unrecovered
+        
+        
         ## Death ##
         if self.INFECTED_ARR[self.currentTimeStep % self.LIFESPAN] > 0:
             recovered = int(self.INFECTED_ARR[self.currentTimeStep % self.LIFESPAN] * self.RECOVER_PROBABILITY)
             self.TOTAL_INFECTED -= self.INFECTED_ARR[self.currentTimeStep % self.LIFESPAN]
             self.TOTAL_RECOVERED += recovered
+            self.RECOVERED_ARR[self.currentTimeStep % self.REINFECTION] = recovered
             self.TOTAL_DEAD += self.INFECTED_ARR[self.currentTimeStep % self.LIFESPAN] - recovered
         
         ## Infection ##
@@ -94,7 +106,7 @@ class Cell(object):
         self.INFECTED_ARR[self.currentTimeStep % self.LIFESPAN] = infected
         self.TOTAL_SUSCEPTIBLE -= infected
         
-        return self.TOTAL_DEAD, self.TOTAL_INFECTED, (self.TOTAL_SUSCEPTIBLE + self.TOTAL_RECOVERED), self.TOTAL_RECOVERED, self.TOTAL_SUSCEPTIBLE
+        return self.TOTAL_DEAD, self.TOTAL_INFECTED, (self.TOTAL_SUSCEPTIBLE + self.TOTAL_INFECTED + self.TOTAL_RECOVERED), self.TOTAL_RECOVERED, self.TOTAL_SUSCEPTIBLE
         
     """
     infect_rate
